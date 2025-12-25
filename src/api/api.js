2aec46abb8cfba1673523cc877e1e43caa223b44
Satyam <tiwarisatyamr10@gmail.com>
2025-12-26 01:06:46 +0530

    Fix API baseURL for production

--- src/api/api.js
+++ src/api/api.js
@@ -1,15 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8000/api/",
-});
-
-api.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
+  baseURL: "https://resume-jd-matcher-backend.onrender.com/api/",
 });
 
 export default api;

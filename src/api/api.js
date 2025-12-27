f7ebf177dbbf42524e9ff3fba8e277f8e2e3ad9d
Satyam Tiwari <satyamtiwarir10@gmail.com>
2025-12-27 22:22:07 +0530

    Update API base URL to new endpoint

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://resume-jd-matcher-backend.onrender.com/api/",
+  baseURL: "https://resume-jd-matcher-backend-1.onrender.com/api/",
 });
 
 api.interceptors.request.use(
